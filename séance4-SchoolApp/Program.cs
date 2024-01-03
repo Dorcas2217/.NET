4a7902c9e407c5693c3800cec64c1fa9943b39df
dorcasMichelle <dorcas.kuezenounga@student.vinci.be>
2024-01-03 11:55:51 +0100

    add of c - requête query

--- séance4-SchoolApp/Program.cs
+++ séance4-SchoolApp/Program.cs
@@ -4,113 +4,132 @@ using SchoolApp.Repositories;
 using SchoolApp.UnitOfWork;
 using System;
 
-    SchoolContext context = new SchoolContext();
-    IUnitOfWorkSchool unitOfWorkSchool = new UnitOfWorkSQL(context);
+SchoolContext context = new SchoolContext();
+IUnitOfWorkSchool unitOfWorkSchool = new UnitOfWorkSQL(context);
 
-    bool exit = false;
+bool exit = false;
 
-    while (!exit)
+while (!exit)
+{
+    Console.WriteLine("MENU:");
+    Console.WriteLine("1 - Ajouter une section");
+    Console.WriteLine("2 - Ajouter un étudiant");
+    Console.WriteLine("3 - Liste des sections");
+    Console.WriteLine("4 - Liste des etudiants ");
+    Console.WriteLine("5 - Afficher les étudiants par section triée par ordre de résultat");
+    Console.WriteLine("6 - Quitter");
+
+    Console.Write("Entrez votre choix  : ");
+    string userInput = Console.ReadLine();
+
+    switch (userInput)
     {
-        Console.WriteLine("MENU:");
-        Console.WriteLine("1 - Ajouter une section");
-        Console.WriteLine("2 - Ajouter un étudiant");
-        Console.WriteLine("3 - Liste des sections");
-        Console.WriteLine("4 - Liste des etudiants ");
-        Console.WriteLine("5 - Quitter");
+        case "1":
+            Console.Write("Nom de la section : ");
+            string sectionName = Console.ReadLine();
 
-        Console.Write("Entrez votre choix  : ");
-        string userInput = Console.ReadLine();
+            Section newSection = new Section { Name = sectionName };
 
-        switch (userInput)
-        {
-            case "1":
-                Console.Write("Nom de la section : ");
-                string sectionName = Console.ReadLine();
+            bool sectionAdded = unitOfWorkSchool.SectionRepository.Save(newSection, s => s.Name.Equals(sectionName));
 
-                Section newSection = new Section { Name = sectionName };
+            if (!sectionAdded)
+            {
+                Console.WriteLine("La section existe déjà.");
+            }
+            else
+            {
+                Console.WriteLine("Section créée avec succès.");
+            }
+            break;
 
-                bool sectionAdded = unitOfWorkSchool.SectionRepository.Save(newSection, s => s.Name.Equals(sectionName));
+        case "2":
 
-                if (!sectionAdded)
-                {
-                    Console.WriteLine("La section existe déjà.");
-                }
-                else
-                {
-                    Console.WriteLine("Section créée avec succès.");
-                }
+            Console.WriteLine("Prénom de l'étudiant:  ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Nom  de l'étudiant:  ");
+            string firstname = Console.ReadLine();
+            Console.WriteLine("Résultat de l'année académique : ");
+            string yearResult = Console.ReadLine();
+            Console.WriteLine("Section de l'étudiant:  ");
+            string section = Console.ReadLine();
+
+            Section sectionFind = unitOfWorkSchool.SectionRepository.SearchFor(s => s.Name.Equals(section)).FirstOrDefault();
+
+            if (sectionFind == null)
+            {
+                Console.WriteLine("Cette section n'existe pas ");
                 break;
+            }
 
-            case "2":
+            Student student = new Student
+            {
+                Name = name,
+                Firstname = firstname,
+                Section = sectionFind,
+                YearResult = long.Parse(yearResult)
 
-                Console.WriteLine("Prénom de l'étudiant:  ");
-                string name = Console.ReadLine();
-                Console.WriteLine("Nnom  de l'étudiant:  ");
-                string firstname = Console.ReadLine();
-                Console.WriteLine("Résultat de l'année académique : ");
-                string yearResult = Console.ReadLine();
-                Console.WriteLine("Section de l'étudiant:  ");
-                string section = Console.ReadLine();
+            };
 
-                Section sectionFind = unitOfWorkSchool.SectionRepository.SearchFor(s => s.Name.Equals(section)).FirstOrDefault();
+            bool rslt = unitOfWorkSchool.StudentRepository.Save(student, stud => stud.Name.Equals(name) && stud.Firstname.Equals(firstname));
 
-                if (sectionFind == null)
+            if (!rslt)
+            {
+                Console.WriteLine("Étudiant existe déjà !");
+            }
+            else
+            {
+                Console.WriteLine("l'Étudiant a été rajouté avec succès !");
+            }
+            break;
+
+        case "3":
+            Console.WriteLine("Liste des sections : ");
+
+            foreach (Section sect in unitOfWorkSchool.SectionRepository.GetAll())
+            {
+                Console.WriteLine(sect.Name);
+            }
+            break;
+
+        case "4":
+            Console.WriteLine(" Liste des etudiants  ");
+
+            foreach (Student stu in unitOfWorkSchool.StudentRepository.GetAll())
+            {
+                Console.WriteLine($" => {stu.Name}, {stu.Firstname}, section {stu.Section.Name}");
+            }
+            break;
+
+        case "5":
+            Console.WriteLine("Liste des étudiants par sections ");
+            var studentsBySection = unitOfWorkSchool.StudentRepository.GetAll()
+                                    .OrderByDescending(student => student.YearResult)
+                                    .GroupBy(student => student.Section)
+                                    .ToList();
+
+            foreach (var group in studentsBySection)
+            {
+                Console.WriteLine($"Section: {group.Key.Name}");
+
+                foreach (Student stud in group.OrderByDescending(s => s.YearResult))
                 {
-                    Console.WriteLine("Cette section n'existe pas ");
-                    break;
+                    Console.WriteLine($"  Student: {stud.Name}, {stud.Firstname}, Result: {stud.YearResult}");
+
                 }
-          
+            }
 
+            break;
 
-                Student student = new Student
-                {
-                    Name = name,
-                    Firstname = firstname,
-                    Section = sectionFind,
-                    YearResult = long.Parse(yearResult)
+        case "6":
+            exit = true;
+            break;
 
-                };
-
-                bool rslt = unitOfWorkSchool.StudentRepository.Save(student, stud =>  stud.Name.Equals(name) && stud.Firstname.Equals(firstname) ) ;
-
-                if (!rslt)
-                {
-                    Console.WriteLine("Étudiant existe déjà !");
-                }
-                else
-                {
-                    Console.WriteLine("l'Étudiant a été rajouté avec succès !");
-                }
-                break;
-
-            case "3":
-                Console.WriteLine("Liste des sections : ");
-
-                foreach(Section sect in unitOfWorkSchool.SectionRepository.GetAll())
-                {
-                    Console.WriteLine(sect.Name);
-                }
-                break;
-
-            case "4":
-                Console.WriteLine(" Liste des etudiants  ");
-
-                foreach(Student stu in unitOfWorkSchool.StudentRepository.GetAll())
-                {
-                    Console.WriteLine($" => {stu.Name}, {stu.Firstname}, section {stu.Section.Name}");
-                }
-                break;
-
-            case "5":
-                exit = true;
-                break;
-
-            default:
-                Console.WriteLine("Choix invalide. Veuillez entrer 1, 2, 3, 4, ou 5.");
-                break;
-        }
-
-        Console.WriteLine(); 
+        default:
+            Console.WriteLine("Choix invalide. Veuillez entrer 1, 2, 3, 4, ou 5.");
+            break;
     }
 
-    Console.WriteLine("Fin du programme.");
+    Console.WriteLine();
+}
+
+Console.WriteLine("Fin du programme.");
